d6002973e43f5d9dec6c420b5a5aad8c38845178
mfons <michael.fons@gmail.com>
2025-01-29 21:49:58 -0700

    additional unit test for transfer

--- src/test/java/com/michaelfons/ferguson_backend_coding_challenge/services/TransferServiceTest.java
+++ src/test/java/com/michaelfons/ferguson_backend_coding_challenge/services/TransferServiceTest.java
@@ -2,7 +2,7 @@ package com.michaelfons.ferguson_backend_coding_challenge.services;
 
 import com.michaelfons.ferguson_backend_coding_challenge.model.Account;
 import com.michaelfons.ferguson_backend_coding_challenge.model.AccountCreationStatus;
-import com.michaelfons.ferguson_backend_coding_challenge.model.Customer;
+import com.michaelfons.ferguson_backend_coding_challenge.model.TransferResults;
 import com.michaelfons.ferguson_backend_coding_challenge.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,10 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -24,29 +25,63 @@ public class TransferServiceTest {
     private TransferService transferService;
     @MockitoBean
     private AccountRepository accountRepository;
-    private Integer sourceAccount;
-    private Integer targetAccount;
+    private Integer sourceAccountId;
+    private Integer targetAccountId;
+    @MockitoBean
+    private Account sourceAccount;
+    private Double sourceAccountBalance;
+    @MockitoBean
+    private Account targetAccount;
+    private Double targetAccountBalance;
+
     private Double amount;
 
     @BeforeEach
     void setUp() {
-        sourceAccount = 1;
-        targetAccount = 2;
+        sourceAccountId = 1;
+        targetAccountId = 2;
         amount = 50.00;
+        sourceAccountBalance = 100.00;
+        targetAccountBalance = 250.00;
+
     }
 
-//    @Test
-//    void aCustomerWantsATransfer_theTransferFunctionalityIsInvoked_() {
-//        // Arrange
-//        var newAccount = new Account(1, aCustomer, initialDeposit);
-//        when(accountRepository.save(any(Account.class))).thenReturn(newAccount);
-//
-//        // Act
-//        var result = transferService.transfer(sourceAccount, targetAccount, amount);
-//
-//        // Assert
-//        assertEquals(result, AccountCreationStatus.SUCCESS);
-//        verify(accountRepository).save(any(Account.class));
-//    }
+    /**
+     * example Unit test for TransferService.transfer
+     */
+    @Test
+    void aCustomerWantsATransfer_moneyIsTransferred_rightRepoMethodsInvoked() {
+        // Arrange
+        when(accountRepository.findById(sourceAccountId)).thenReturn(Optional.ofNullable(sourceAccount));
+        when(accountRepository.findById(targetAccountId)).thenReturn(Optional.ofNullable(targetAccount));
+        doNothing().when(sourceAccount).setBalance(sourceAccountBalance - amount);
+        doNothing().when(targetAccount).setBalance(targetAccountBalance + amount);
+        Account alteredSourceAccount = new Account(sourceAccountId, null, sourceAccountBalance - amount);
+        Account alteredTargetAccount = new Account(targetAccountId, null, targetAccountBalance + amount);
+        when(accountRepository.save(sourceAccount)).thenReturn(alteredSourceAccount);
+        when(accountRepository.save(targetAccount)).thenReturn(alteredTargetAccount);
+        when(sourceAccount.getId()).thenReturn(sourceAccountId);
+        when(targetAccount.getId()).thenReturn(targetAccountId);
+        when(sourceAccount.getBalance()).thenReturn(sourceAccountBalance).thenReturn(sourceAccountBalance).thenReturn(alteredSourceAccount.getBalance()).thenReturn(alteredSourceAccount.getBalance());
+        when(targetAccount.getBalance()).thenReturn(targetAccountBalance).thenReturn(alteredTargetAccount.getBalance()).thenReturn(alteredTargetAccount.getBalance());
+
+        // Act
+        var result = transferService.transfer(sourceAccountId, targetAccountId, amount);
+
+        // Assert
+        assertEquals(result, new TransferResults(1, sourceAccountId, sourceAccountBalance, sourceAccountBalance - amount,
+                targetAccountId, targetAccountBalance, targetAccountBalance + amount));
+        verify(accountRepository).findById(sourceAccountId);
+        verify(accountRepository).findById(targetAccountId);
+        verify(sourceAccount, times(4)).getBalance();
+        verify(targetAccount, times(3)).getBalance();
+        verify(sourceAccount).setBalance(sourceAccountBalance - amount);
+        verify(targetAccount).setBalance(targetAccountBalance + amount);
+        verify(accountRepository).save(sourceAccount);
+        verify(accountRepository).save(targetAccount);
+        verify(sourceAccount).getId();
+        verify(targetAccount).getId();
+
+    }
 
 }
